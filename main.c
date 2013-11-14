1bbd3b9ce4ce001c3ba092a65346216a3fd0bfae
Andy Armstrong <andy@hexten.net>
2013-11-14 16:28:28 +0000

    Simulate channels, run in RT.

--- main.c
+++ main.c
@@ -2,15 +2,50 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "crono.h"
 #include "utils.h"
 
+#define CHANNELS 4
+
+static int temp[CHANNELS];
+
+static void parse_action(const char *action, int *channel, int *temp) {
+  const char *sp = action;
+  char *ep;
+  if (*sp == '"') sp++;
+  *channel = strtoul(sp, &ep, 10);
+  if (sp == ep) die("Bad channel");
+  sp = ep;
+  *temp = strtoul(sp, &ep, 10);
+  if (sp == ep) die("Bad temperature");
+  sp = ep;
+  if (*action == '"' && *sp == '"') sp++;
+  if (*sp) die("Bad action");
+}
+
 static int action(void *ctx, const char *action, time_t when) {
-  printf("Fired %s\n", action);
+  int ch, tp;
+  (void) ctx;
+  (void) when;
+  parse_action(action, &ch, &tp);
+  if (ch < 1 || ch > CHANNELS) die("Bad channel: %d", ch);
+  temp[ch - 1] = tp;
   return 0;
+}
+
+static void dump_temp(int *temp, int channels) {
+  for (int i = 0; i < channels; i++) {
+    if (i) printf(", ");
+    printf("%02d: ", i + 1);
+    if (temp[i] < 0) printf("???");
+    else printf("%3d", temp[i]);
+  }
+  printf("\n");
 }
 
 static crono_rule *read_file(crono_rule *rules, FILE *ct, struct tm *tm) {
@@ -67,20 +102,40 @@ static void dump_rules(crono_rule *rules) {
   }
 }
 
+static void ts(char *s, size_t max) {
+  time_t now = time(NULL);
+  struct tm tm;
+  localtime_r(&now, &tm);
+  strftime(s, max, "%Y/%m/%d %H:%M:%S", &tm);
+}
+
 int main(int argc, char *argv[]) {
   crono_rule *rules = NULL;
   struct tm tm;
-  time_t t = get_time(&tm);
+  char tsbuf[100];
+  int i;
 
-  for (int i = 1; i < argc; i++)  {
+  get_time(&tm);
+
+  for (i = 1; i < argc; i++)
     rules = read_crontab(rules, argv[i], &tm);
+
+  for (i = 0; i < CHANNELS; i++)
+    temp[i] = -1;
+
+  for (;;) {
+    time_t now = time(NULL);
+    /*    dump_rules(rules);*/
+    rules = crono_rule_trigger(rules, now);
+
+    ts(tsbuf, sizeof(tsbuf));
+    printf("%s ", tsbuf);
+    dump_temp(temp, CHANNELS);
+
+    unsigned snooze = rules->tm - now;
+    printf("(sleeping for %u seconds)\n", snooze);
+    sleep(snooze);
   }
-
-  dump_rules(rules);
-
-  rules = crono_rule_trigger(rules, t);
-
-  dump_rules(rules);
 
   crono_rule_free(rules);
   return 0;
